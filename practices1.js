973993b98dfdb8d8a39d6b6db902b02df0de2758
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-06-29 13:49:51 +0530

    first commit

---
+++ practices1.js
@@ -0,0 +1,4 @@
+//Uppercase Array
+const strings = ["avengers", "captain america", "iron man", "black panther"];
+const uppercased = strings.map(strings => strings.toUpperCase());
+console.log(uppercased);
