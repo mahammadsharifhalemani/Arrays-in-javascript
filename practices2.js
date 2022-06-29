e7d0c820fbbdd86090c97af5b2db36a55b67a42b
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-06-29 13:55:35 +0530

    second commit

---
+++ practices2.js
@@ -0,0 +1,4 @@
+//filtering array
+const inputWords = ["spray", "limit", "elite", "exuberant","destruction", "present"];
+const results = inputWords.filter(inputWords =>inputWords.length>5);
+console.log(results);
